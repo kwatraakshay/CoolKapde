4d1966c99d9bc127abcbd7204bddeffe5bd91f6e
Akshay Kwatra <akshaykwatra261998@gmail.com>
2023-08-08 11:27:12 +0530

    intital commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+//use rafce for the shortcut to create the arrow function
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div align='center'>Welcome to Proshop</div>
+    <span align='center'>i am span</span>
+    <span>another span</span>
+    </>
+  )
+}
+
+export default App
