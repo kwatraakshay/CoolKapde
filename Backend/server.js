f9c254ed312e888a6af2638913d8d7f406bf02eb
kwatraakshay <akshaykwatra261998@gmail.com>
2023-10-02 16:01:29 +0530

    working on local

--- Backend/server.js
+++ Backend/server.js
@@ -1,53 +1,61 @@
 import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-dotenv.config();
+import userRoutes from './routes/userRoutes.js';
+import  dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import { notFound,errorHandler } from './middleware/errorMiddelware.js';
 import uploadRoutes from './routes/uploadRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 
-const port = process.env.PORT || 5000;
+dotenv.config();
 
+const port = process.env.PORT || 8120;
 connectDB();
 
-const app = express();
+const app = express()
 
+//body parser middle ware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
+
 app.use(cookieParser());
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);
+app.use(cors());
 
-app.get('/api/config/paypal', (req, res) =>
-  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
-);
+// respond with "hello world" when a GET request is made to the homepage
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
+app.use('/api/orders',orderRoutes);
+app.use('/api/upload',uploadRoutes);
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
+
+app.get('/api/config/paypal',(req,res) => 
+res.send({clientId:
+process.env.PAYPAL_CLIENT_ID}));
+
+const __dirname = path.resolve();
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+    //any route that is not api will be redirected to index.html
+    app.get('*',(req,res) => 
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html')));
 } else {
-  const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
+    app.get('/',(req,res) => {
+        res.send('API is running...');
+    });
 }
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
-);
+
+
+app.listen(port,()=>console.log(`Server running on port ${port}`));
